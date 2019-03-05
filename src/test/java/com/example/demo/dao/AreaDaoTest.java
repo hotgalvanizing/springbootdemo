81aabbc0c243c8d876e81d00afd60a6ba06d54f6
hotgalvanizing <35394878+hotgalvanizing@users.noreply.github.com>
2019-03-05 22:58:32 +0800

    first

---
+++ src/test/java/com/example/demo/dao/AreaDaoTest.java
@@ -0,0 +1,25 @@
+package com.example.demo.dao;
+
+import com.example.demo.entity.Area;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AreaDaoTest {
+
+    @Autowired
+    private AreaDao areaDao;
+
+    @Test
+    public void queryArea(){
+        List<Area> areaList = areaDao.queryArea();
+        assertEquals(3,areaList.size());
+    }
+}
